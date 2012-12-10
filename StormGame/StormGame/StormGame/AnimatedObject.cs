efb103795cefa6003f8aac36c271bd3a27751a69
Bob <bob.rathgeber@gmail.com>
2012-12-10 18:05:57 -0500

    Changed Animated Object framerate
    from 60-30

--- StormGame/StormGame/StormGame/AnimatedObject.cs
+++ StormGame/StormGame/StormGame/AnimatedObject.cs
@@ -12,7 +12,7 @@ namespace StormGame
             Texture = Globals.Content.Load<Texture2D>(Identifier);
             Dictionary<string, Rectangle> spriteMap = Globals.Content.Load<Dictionary<string, Rectangle>>(Identifier + "SpriteMap");
             animation = new Animation(Texture, spriteMap);
-            animationPlayer.PlayAnimation(animation, "Idle", 0.1f, true);
+            animationPlayer.PlayAnimation(animation, "Idle", 0.2f, true);
         }        
     }
 }
